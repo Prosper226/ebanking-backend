8aef1c9e6edf72bcc5f45f3d2991fa8c957b3407
Prosper226 <psedgo@gmail.com>
2024-02-16 10:34:01 +0000

    first commit

---
+++ src/main/java/org/sid/ebankingbackend/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package org.sid.ebankingbackend.exceptions;
+
+public class CustomerNotFoundException extends Exception {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+}
